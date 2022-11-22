8df9db13710737887add35498c7dcd10117b157d
kraemersimon <simon.kraemer.2007@gmail.com>
2022-11-22 17:33:39 +0100

    working linefollower

--- main/main.ino
+++ main/main.ino
@@ -8,36 +8,43 @@ EVShield evshield(0x34, 0x36);
 EVs_EV3Color colorR;  //erster  Farbsensor heisst colorR
 EVs_EV3Color colorL;  //zweiter Farbsensor heisst colorL
 
-int SPEED = 15;
-int MULTIPLIKATOR = 8;
+int SPEED = 255;
 void setup() {
+  pinMode(2, OUTPUT);   //Pinout linker Motor
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);   //Pinout rechter Motor
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+
   evshield.init(SH_HardwareI2C);
 
-  colorR.init(&evshield, SH_BAS1);          //rechter Farbsensor ist an Port BAS1
+  colorR.init(&evshield, SH_BBS1);          //rechter Farbsensor ist an Port BBS1
   colorR.setMode(MODE_Color_MeasureColor);  //rechter Farbsensor sucht nach Farbe
 
-  colorL.init(&evshield, SH_BBS1);          //linker Farbsensor ist an Port BBS1
+  colorL.init(&evshield, SH_BBS2);          //linker Farbsensor ist an Port BBS
   colorL.setMode(MODE_Color_MeasureColor);  //linker Farbsensor sucht nach Farbe
 
   // Warten auf GO-Knopf
   Serial.begin(9600);
-  Serial.println("Start");
+  //Serial.println("Start");
   while (!evshield.getButtonState(BTN_GO)) {}
   evshield.bank_a.ledSetRGB(0, 0, 0);
   evshield.bank_b.ledSetRGB(0, 0, 0);
 }
 
 void loop() {
-  int farbeR = colorR.getVal();
-  int farbeL = colorL.getVal();
+  int r = colorR.getVal();
+  int l = colorL.getVal();
 
-  int delta = farbeL - farbeR;
-  int s = delta * MULTIPLIKATOR;
-  fahreOhneBremse(s + SPEED, -s + SPEED, 1);
 
-  
+  if ((l == 6 && r == 6) || (l == 1 && r == 1)) m(SPEED, SPEED, 0);
+  else if (l == 1) m(SPEED, -SPEED, 0);
+  else if (r == 1) m(-SPEED, SPEED, 0);
 
-  if (farbeR == 3) {
+
+ /* if (farbeR == 3) {
     fahre(0, 0, 1);
     delay(2000);
     fahre(40, 40, 70);
@@ -61,7 +68,7 @@ void loop() {
     evshield.bank_a.ledSetRGB(0, 0, 100);
     while (colorL.getVal() == 6) fahreOhneBremse(-50, 0, 1);
     fahre(-100, 100, 50);
-  }
+  }*/
   
 }
 
@@ -69,17 +76,37 @@ void loop() {
               Funktionen
 ********************************************/
 
+void m(int left, int right, int duration) {
+  if (left < -255) left = -255;
+  if (left > 255) left = 255;
+  if (right < -255) right = -255;
+  if (right > 255) right = 255;
 
-void fahre(int links, int rechts, int zeit) {
-  evshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, -links);
-  evshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, -rechts);
-  delay(zeit);
-  evshield.bank_b.motorStop(SH_Motor_1, SH_Next_Action_Brake);
-  evshield.bank_b.motorStop(SH_Motor_2, SH_Next_Action_Brake);
-}
+  if (left > 0) {
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+  }
+  else {
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+  }
 
-void fahreOhneBremse(int links, int rechts, int zeit) {
-  evshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, -links);
-  evshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, -rechts);
-  delay(zeit);
+  if (right > 0) {
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+  }
+  else {
+    digitalWrite(2, HIGH);
+    digitalWrite(4, LOW);
+  }
+
+  analogWrite(5, abs(left));
+  analogWrite(3, abs(right));
+  delay(duration);
+  if (duration != 0) stop();
+}  
+
+void stop() {
+  analogWrite(5, 0);
+  analogWrite(3, 0);
 }
